8a40169b30277eb74632b0d3eb25991343ca295e
Khristina Tastaeva <khristinach@gmail.com>
2022-08-06 01:09:58 +0300

    Completed homework 1.2

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
@@ -13,5 +14,13 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            p = Phone(
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=re.sub('\s', '-', phone['name']).lower()
+            )
+            p.save()
+
